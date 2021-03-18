30c45cb2891fcd2289be3598cf16740a88221af1
Donatas Stražisnkas <donat.straz@gmail.com>
2021-03-18 05:28:53 +0200

    pritaikiau puslapi mazesniems ekranams

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-const btns = document.querySelectorAll(".btn");
+const btns = document.querySelectorAll(".button");
 const books = document.querySelectorAll(".book-card");
 
 for(i= 0; i < btns.length; i++){
@@ -19,21 +19,27 @@ for(i= 0; i < btns.length; i++){
     })
 }
 
+//Get input element
+let filterInput = document.getElementById('search');
+//EventListener
+filterInput.addEventListener('keyup', filterNames);
 
-//Filtras (blogas)
-const search = document.getElementById("search");
+function filterNames(){
+	let filterValue = document.getElementById('search').value.toUpperCase();
+	//Get names ul
+	let ul = document.getElementById('books');
+	//Get items
+	let li = ul.querySelectorAll('li.book-card');
 
-search.addEventListener("keyup", (e) => {
-    e.preventDefault();
-    const searchValue = search.value.toLowerCase().trim();
+	//Loop through collection-item list
+	for(let i = 0;i < li.length;i++){
+		let a = li[i].getElementsByTagName('a')[0];
+		// If matched
+		if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+			li[i].style.display = '';
+		} else {
+		    li[i].style.display = 'none';
+		}
 
-    for(i = 0; i < books.length; i++){
-        if(books[i].classList.contains(searchValue)){
-            books[i].style.display = "block";
-        }else if(searchValue == ""){
-            books[i].style.display = "block";
-        }else{
-        books[i].style.display = "none";
-        }
-    }
-})
+	}
+}
