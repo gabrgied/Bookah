f5b558ca02914dff734c9b24acf7eef997de4885
Žygimantas <z.kuklys@gmail.com>
2021-03-24 15:19:26 +0200

    pridetas susisiekimas su admin

---
+++ javascript/email.js
@@ -0,0 +1,19 @@
+ (function () {
+     // https://dashboard.emailjs.com/admin/integration
+     emailjs.init('user_H2qPQ4kLOaXHKL5q6lamG');
+ })();
+
+ window.onload = function () {
+     document.getElementById('contact-form').addEventListener('submit', function (event) {
+         event.preventDefault();
+         // generate a five digit number for the contact_number variable
+         this.contact_number.value = Math.random() * 100000 | 0;
+         // these IDs from the previous steps
+         emailjs.sendForm('service_01u1n9g', 'template_rab8k5h', this)
+             .then(function () {
+                 alert("Jūsų žinutė buvo išsiųsta.");
+             }, function (error) {
+                 alert("Nepavyko išsiųsti žinutės");
+             });
+     });
+ }
