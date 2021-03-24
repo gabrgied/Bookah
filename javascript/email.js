4e2d4d3cacf5a155666a3885f397b8c65c23c8e4
Žygimantas <z.kuklys@gmail.com>
2021-03-24 18:45:48 +0200

    sutvarkytas duomenu isvalymas po submit

--- javascript/email.js
+++ javascript/email.js
@@ -12,6 +12,7 @@
          emailjs.sendForm('service_01u1n9g', 'template_rab8k5h', this)
              .then(function () {
                  alert("Jūsų žinutė buvo išsiųsta.");
+                 document.getElementById("contact-form").reset();
              }, function (error) {
                  alert("Nepavyko išsiųsti žinutės");
              });
